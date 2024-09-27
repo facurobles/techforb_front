b97cc99e4efc90c0c85443f453d44c6f89e55b20
facurobles <hfacu.robles@gmail.com>
2024-09-26 21:22:09 -0300

    lado izquierdo del loguin creado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoguinComponent } from './features/loguin/components/loguin/loguin.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'loguin', pathMatch: 'full'},
+  {path: 'loguin', component: LoguinComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
