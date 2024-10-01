0d1bb0ff3e20d3a90d063b10533fecc01f7f412f
facurobles <hfacu.robles@gmail.com>
2024-10-01 14:55:28 -0300

    creracion de guard para protejer las rutas

---
+++ src/app/core/guards/autenticacion.guard.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AutenticacionGuard implements CanActivate {
+
+  constructor(private cookieService: CookieService, private router: Router) { }
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+
+    const token = this.cookieService.get('token');
+
+    if (token) {
+      console.log('el guard me dejo pasar a la ruta')
+      return true;
+    }else{
+      console.log('se redirigió al logue}in por falta de token')
+      this.router.navigate(['/login']);
+      return false;
+    }
+
+  }
+
+}
