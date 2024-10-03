3ed8ddcfaacc699794fe1b1d9060b49f31527bef
facurobles <hfacu.robles@gmail.com>
2024-10-03 10:20:16 -0300

    agrergo endponits para CRUD clase Planta

--- src/app/core/services/backend-url.service.ts
+++ src/app/core/services/backend-url.service.ts
@@ -11,6 +11,7 @@ export class BackendUrlService {
 
   constructor() { }
 
+  /**ENDPOINTS USUARIO*/
   public getLoginUrl():string{
     return this.url + 'autenticacion/login'
   }
@@ -26,5 +27,24 @@ export class BackendUrlService {
   public getBorrarUsuarioUrl(email : string):string{
     return `${this.url}usuario/borrar/${email}`;
   }
+
+
+  /**ENDPOINTS PLANTA*/
+  public getCrearPlantaUrl():string{
+    return `${this.url}planta/crear`;
+  }
+
+  public getEliminarPlantaUrl(id: string):string{
+    return `${this.url}planta/eliminar/${id}`;
+  }
+    
+  public getActualizarPlantaUrl(id: string):string{
+    return `${this.url}planta/actualizar/${id}`;
+  }
+  public getMetricasPlantasUrl():string{
+    return `${this.url}planta/obtenerMetricas`;
+  }
+
+
 
 }
